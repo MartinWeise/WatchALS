7f352313a56bb6aed9ec0c12573985f3d93a8977
Martin Weise <martin.weise@tuwien.ac.at>
2021-05-07 10:13:52 +0200

    better labels and better summary including the total dosage

--- app/src/main/java/at/weise/cpr/AlsActivity.kt
+++ app/src/main/java/at/weise/cpr/AlsActivity.kt
@@ -1,6 +1,9 @@
 package at.weise.cpr
 
+import android.content.Context
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.View
 import android.widget.Button
 import android.text.Html
@@ -14,12 +17,14 @@ class AlsActivity : FragmentActivity() {
     private var ticks = 0
     private var analyses = 0
     private var shocks = 0
-    private var adrenalin = 0
-    private var amiodaron = 0
+    private var adrenaline = 0
+    private var amiodarone = 0
     private lateinit var timer: Timer
     private lateinit var cprTime: TextView
+    private lateinit var vibrator: Vibrator
     private lateinit var cprCycles: TextView
-    private lateinit var cprAls: TextView
+    private lateinit var cprAlsAdrenaline: TextView
+    private lateinit var cprAlsAmiodarone: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +32,10 @@ class AlsActivity : FragmentActivity() {
         /* make timer available */
         cprTime = findViewById(R.id.timer)
         cprCycles = findViewById(R.id.cycles)
-        cprAls = findViewById(R.id.als)
+        cprAlsAdrenaline = findViewById(R.id.als_adrenaline)
+        cprAlsAmiodarone = findViewById(R.id.als_amiodarone)
+        /* vibrator */
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         /* better labels */
         findViewById<Button>(R.id.amiodaron).setText(Html.fromHtml("Amio.<sup>150mg</sup>"))
         findViewById<Button>(R.id.adrenalin).setText(Html.fromHtml("Adr.<sup>1mg</sup>"))
@@ -49,20 +57,20 @@ class AlsActivity : FragmentActivity() {
     }
 
     fun adrenalinApplied(view: View) {
-        adrenalin++
+        adrenaline++
         showALS()
     }
 
     fun amiodaronApplied(view: View) {
-        amiodaron++
+        amiodarone++
         showALS()
     }
 
     private fun showStatistic() {
         cprCycles.visibility = View.VISIBLE
-        var textShocks = "Schock"
-        if (shocks > 1) {
-            textShocks += "s"
+        var textShocks = getString(R.string.shock)
+        if (shocks != 1) {
+            textShocks = getString(R.string.shock_multiple)
         }
         runOnUiThread {
             cprCycles.text = "" + shocks + " " + textShocks + " (+" + (analyses - shocks) + ")"
@@ -71,40 +79,55 @@ class AlsActivity : FragmentActivity() {
 
     private fun showALS() {
         /* text */
-        if (amiodaron > 0 || adrenalin > 0) {
-            cprAls.visibility = View.VISIBLE
+        if (amiodarone > 0) {
+            cprAlsAmiodarone.visibility = View.VISIBLE
+        }
+        if (adrenaline > 0) {
+            cprAlsAdrenaline.visibility = View.VISIBLE
         }
         /* buttons */
-        if (shocks >= 3 && amiodaron != 3) {
+        if (shocks >= 3 && amiodarone != 3) {
             findViewById<Button>(R.id.amiodaron).visibility = View.VISIBLE
         }
-        if (amiodaron == 3) {
+        if (amiodarone == 3) {
             findViewById<Button>(R.id.amiodaron).visibility = View.INVISIBLE
         }
-        if (analyses > 0 && adrenalin != 10) {
+        if (analyses > 0 && adrenaline != 10) {
             findViewById<Button>(R.id.adrenalin).visibility = View.VISIBLE
         }
-        if (adrenalin == 10) {
+        if (adrenaline == 10) {
             findViewById<Button>(R.id.adrenalin).visibility = View.INVISIBLE
         }
         runOnUiThread {
-            cprAls.text = "" + adrenalin + "x Adr + " + amiodaron + "x Amio"
+            if (adrenaline > 0) {
+                cprAlsAdrenaline.text = "" + adrenaline + "x " + getString(R.string.adrenaline_short) + " (" + adrenaline + "mg)"
+            }
+            if (amiodarone > 0) {
+                cprAlsAmiodarone.text = "" + amiodarone + "x " + getString(R.string.amiodarone_short) + " (" + (amiodarone * 150) + "mg)"
+            }
         }
     }
 
     private fun startTimer() {
         timer = fixedRateTimer(name = "CPR-Timer", initialDelay = 0, period = 1000) {
             ticks++
+            if (ticks != 0 && ticks % (60*4) == 0) {
+                vibrate()
+            }
             runOnUiThread {
                 cprTime.text = formatTime(ticks)
             }
         }
     }
 
+    private fun vibrate() {
+        vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+
     private fun formatTime(totalSeconds: Int): String {
         val minutes = Math.floorDiv(totalSeconds, 60)
         val seconds = totalSeconds - minutes * 60
-        return "REA " + String.format("%02d", minutes) + ":" + String.format("%02d", seconds)
+        return getString(R.string.cpr) + String.format(" %02d", minutes) + ":" + String.format("%02d", seconds)
     }
 
     private fun stopTimer() {
